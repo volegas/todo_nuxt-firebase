ded75334d68cc40ac756aa8628ca2a7aed63a1eb
volegas <109149987+volegas@users.noreply.github.com>
2022-09-01 16:11:39 +0000

    Fix Typos

--- src/App.vue
+++ src/App.vue
@@ -2,4 +2,5 @@
   <div id="app" class="container-fluid">
     <div class="container-fluid">
         <div>
-          <Topic v-if="state == 0"
+          <Topic v-if="state == 0" v-on:setTopic = "updateTopic($event)"/>
+        
