575c0e42af4614e8ce9fb2c0917dcaa3e03741a9
volegas <109149987+volegas@users.noreply.github.com>
2022-09-02 07:09:17 +0000

    locale: Update compiled localization files

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,4 @@
     <div class="container-fluid">
         <div>
           <Topic v-if="state == 0" v-on:setTopic = "updateTopic($event)"/>
-        
+          <Entry :socket="socket" v-if="state ==
