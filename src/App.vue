a6a08bee8c0728fdba4c4644a44ba01049c43b09
volegas <109149987+volegas@users.noreply.github.com>
2022-09-03 06:32:50 +0000

    project files

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,9 @@
           <Topic v-if="state == 0" v-on:setTopic = "updateTopic($event)"/>
           <Entry :socket="socket" v-if="state == 1" :topic="topic" v-on:setCharacter = "updateCharacter($event)"/>
           <Chat v-if="state == 2" :character="selectedCharacter" :name="name" :socket="socket" :topic="topic"/>
-        </div
+        </div>
+    </div>
+  </div>
+</template>
+
+<script
