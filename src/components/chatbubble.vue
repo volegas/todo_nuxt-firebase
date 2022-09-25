1bce370703242f4f42819fdbabc600e7ff3aef95
volegas <109149987+volegas@users.noreply.github.com>
2022-09-25 16:10:19 +0000

    Rename spec file

--- src/components/chatbubble.vue
+++ src/components/chatbubble.vue
@@ -3,4 +3,5 @@
         <div class="container">
             <div class="bubbleC"> 
                 <div v-bind:style="bubbleStyle" class="bubble">
-                        <div v-bind:style="c
+                        <div v-bind:style="colourStyle">
+                            <h6 v-
