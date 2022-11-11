eaf64b1a1c299e5d9ef07e8618ca34fa87b58f50
volegas <109149987+volegas@users.noreply.github.com>
2022-11-11 16:29:53 +0000

    Add missing license header

--- src/components/entry.vue
+++ src/components/entry.vue
@@ -12,4 +12,4 @@
             </div>
             <div class="charContainer">
                 <div class="characters" v-for="(char,index) in characters" :key="index">
-                    <div class="character-card"  @mouseover="transformButtom(1,index)" @mouseout="transformButtom(2,index)" @click="selectcha
+                    <div class="character-card"  @mouseover="transformButtom(1,index)" @mouseout="transformButtom(2,index)" @click="selectcharacter(index)" :class="{active: activeItem
